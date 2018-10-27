7d57739a70ef4cd8a02b49d7fe9e57ade6e15632
Alessandro Rodi <alessandro.rodi@renuo.ch>
2018-10-27 11:29:18 +0200

    Fix test

--- spec/system/happy_path_spec.rb
+++ spec/system/happy_path_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-
 RSpec.describe 'an happy path' do
   it 'goes through all the website', js: true do
     visit root_path
-    expect(page).to have_content('hello')
+    expect(page).to have_content(I18n.t('title'))
   end
 end
