1cbab136bf75e5a37a92531c635ea86b8f930c97
Lukas Bischof <lukas.bischof@renuo.ch>
2018-11-01 15:17:00 +0100

    Semicolons

--- app/assets/packs/hello_elm.js
+++ app/assets/packs/hello_elm.js
@@ -5,8 +5,8 @@
 import Elm from '../Main'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const target = document.createElement('div')
+  const target = document.createElement('div');
 
-  document.body.appendChild(target)
+  document.body.appendChild(target);
   Elm.Main.embed(target)
-})
+});
