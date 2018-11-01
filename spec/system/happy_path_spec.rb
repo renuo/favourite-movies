853a59b4baddd540e765405c560f2df5ff9191f4
Simon Huber <simon.huber@renuo.ch>
2018-11-01 17:22:46 +0100

    test setup

--- spec/system/happy_path_spec.rb
+++ spec/system/happy_path_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'an happy path' do
-  it 'goes through all the website', js: true, driver: :chrome do
+  it 'goes through all the website', js: true do
     visit root_path
+    find('#searchInput')
     # errors = page.driver.browser.manage.logs.get(:browser)
     expect(page).to have_content(I18n.t('title'))
+  end
+
+  it 'searches', :vcr  do
+    visit root_path
+    find('#searchInput')
+
+    expect(page).to have_content(I18n.t('title'))
+    fill_in 'searchInput', with: "Jurassic"
+
+    expect(page).to have_content(I18n.t('title'))
+    expect(all('mat-card').size).to eq(10)
+    fill_in 'searchInput', with: "Jurassic Park III: Island Attack"
+    expect(all('mat-card').size).to eq(1)
   end
 end
