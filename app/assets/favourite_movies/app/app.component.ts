8a9f739451f196cde4fff97bab8a0247879fb0ac
Martin Cavoj <martin.cavoj@renuo.ch>
2018-11-01 16:39:40 +0100

    Searching

--- app/assets/favourite_movies/app/app.component.ts
+++ app/assets/favourite_movies/app/app.component.ts
@@ -1,18 +1,41 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MovieService} from "./movie.service";
 import {Movie} from "./movie";
 import appTemplate from  './app.template.html';
 import {FormControl} from "@angular/forms";
+import {debounceTime, distinct, distinctUntilChanged, switchMap} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'favourite-movies',
   template: appTemplate
 })
-export class AppComponent {
-  @ViewChild('input') searchInput: FormControl;
+export class AppComponent implements OnInit {
+  // @ViewChild('searchInput') searchInput: ElementRef;
+  // searchInput: FormControl;
+  private subject: Subject<string> = new Subject();
   public movies: Movie[];
 
   constructor(public movieService: MovieService) {
-    movieService.fetchMovies('kill').subscribe(movies => this.movies = movies);
+    // this.searchMovies('');
+    // this.searchInput = new FormControl({ value: '' })
+  }
+
+  private searchMovies(term: string) {
+    console.log('term', term);
+    this.movieService.fetchMovies(term).subscribe(movies => this.movies = movies);
+  }
+
+  public searchKeyUp(term: string) {
+    this.subject.next(term);
+  }
+
+  ngOnInit() {
+    this.subject.pipe(debounceTime(400)).subscribe(term => this.searchMovies(term));
+    // console.log(this.searchInput);
+    // this.searchInput.valueChanges.pipe(
+    //   debounceTime(400),
+    //   distinctUntilChanged(),
+    //   switchMap(term => this.movieService.fetchMovies(term)))
   }
 }
